57219d4ea6e39435a0814238d2a01d197e96d166
subuxd <subhamksingh01@gmail.com>
2024-02-02 01:58:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,91 @@
-import './App.css';
-import {Search, MapPin, Wind} from 'react-feather';
-import getWeather from './api/api';
-import { useState } from 'react';
-import dateFormat from 'dateformat';
+import "./App.css";
+import { Search, MapPin, Wind } from "react-feather";
+import getWeather from "./api/api";
+import { useState } from "react";
+import dateFormat from "dateformat";
 
 function App() {
-
-  const [city,setCity] = useState("");
-  const [weather,setWeather] = useState({});
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
 
   const getWeatherbyCity = async () => {
     const weatherData = await getWeather(city);
     setWeather(weatherData);
-    setCity(""); 
-  }
+    setCity("");
+  };
 
   const renderDate = () => {
     let now = new Date();
     return dateFormat(now, "dddd, mmmm dS, h:MM TT");
-  }
+  };
 
   return (
     <div className="app">
       <h1>Weather App</h1>
+      {/**Search */}
       <div className="input-wrapper">
-        <input type="text" value={city} onChange={(e) => setCity(e.target.value)} 
-        placeholder='Enter City Name' />
-        <button onClick={()=>getWeatherbyCity()}>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter your city"
+        />
+        <button onClick={() => getWeatherbyCity()}>
           <Search></Search>
         </button>
       </div>
 
-      {weather && weather.weather && 
-      <div className="content">
-        
-        <div className="location d-flex">
-          <MapPin></MapPin>
-          <h2>{weather.name} <span>({weather.sys.country})</span></h2>
+      {/**Content after search is performed if data exists*/}
+      {weather && weather.weather && (
+        <div className="content">
+          <div className="location d-flex">
+            <MapPin></MapPin>
+            <h2>
+              {weather.name} <span>({weather.sys.country})</span>
+            </h2>
+          </div>
+          <p className="datetext">{renderDate()}</p>
+
+          <div className="weatherdesc d-flex flex-c">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt=""
+            />
+            <h3>{weather.weather[0].description}</h3>
+          </div>
+
+          <div className="tempstats d-flex flex-c">
+            <h1>
+              {weather.main.temp} <span>&deg;C</span>
+            </h1>
+            <h3>
+              Min. Temp {weather.main.temp_min} <span>&deg;C</span>
+            </h3>
+            <h3>
+              Max. Temp {weather.main.temp_max} <span>&deg;C</span>
+            </h3>
+            <h3>
+              Humidity {weather.main.humidity} <span>%</span>
+            </h3>
+          </div>
+
+          <div className="windstats d-flex">
+            <Wind></Wind>
+            <h3>
+              Wind is {weather.wind.speed} Knots in {weather.wind.deg}&deg;
+            </h3>
+          </div>
         </div>
-        <p className="datetext">{renderDate()}</p>
+      )}
 
-        <div className="weatherdesc d-flex flex-c">
-          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
-          <h3>{weather.weather[0].description}</h3>
+      {/**if data doesn't exist */}
+      {!weather.weather && (
+        <div className="content">
+          <h4>No Data found !</h4>
         </div>
-
-        <div className="tempstats d-flex flex-c">
-          <h1>{weather.main.temp} <span>&deg;C</span></h1>
-          <h3>Feels Like {weather.main.feels_like} <span>&deg;C</span></h3>
-        </div>
-
-        <div className="windstats d-flex">
-          <Wind></Wind>
-          <h3>Wind is {weather.wind.speed} Knots in {weather.wind.deg}&deg;</h3>
-        </div>
-
-      </div>
-      }
-
-      {!weather.weather && <div className="content">
-        <h4>No Data found !</h4>
-      </div>}
+      )}
 
       {/* <p>{JSON.stringify(weather)}</p> */}
-
     </div>
   );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,13 @@
-const apiKey = 'ac65a6c2eca5dd23204f692d004c8c12';
+const apiKey = "e9230c66468d61b1887c8c10446c6026";
 
 const getWeather = async (city) => {
-    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+  return await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  )
     .then((res) => res.json())
     .then((json) => {
-        return json;
-    })
-}
+      return json;
+    });
+};
 
 export default getWeather;
